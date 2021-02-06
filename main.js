bce0363329ac775ee7f225af3439528b9c55fc08
Waldemar Adams <waldek-adamski@wp.pl>
2021-02-06 16:00:04 +0100

    feat: fn 'addNode'- create and add to HTML node

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const formAdd = document.querySelector('.form--add');
+const content = document.querySelector('.content');
 
-const addNode = function (e) {
+const addNode = function (e, node = 'div', txt = "tekst", atr = 'style', atrValue = 'font-size:20px') {
     e.preventDefault();
     console.log('ok');
+
+    const element = document.createElement(node);
+    const text = document.createTextNode(txt);
+    element.setAttribute(atr, atrValue);
+    element.appendChild(text);
+
+    content.appendChild(element)
 }
 
 formAdd.addEventListener('submit', addNode);
