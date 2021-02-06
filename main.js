c61052fb1ab5dea030913c4f92dc932e863f260e
Waldemar Adams <waldek-adamski@wp.pl>
2021-02-06 20:43:57 +0100

    feat:modify 'addNode' fn

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const formAdd = document.querySelector('.form--add');
 const content = document.querySelector('.content');
 
-const addNode = function (e, node = 'div', txt = "tekst", atr = 'style', atrValue = 'font-size:20px') {
+const addNode = function (e, node, txt, atr, atrValue) {
     e.preventDefault();
     console.log('ok');
 
     const element = document.createElement(node);
-    const text = document.createTextNode(txt);
-    element.setAttribute(atr, atrValue);
-    element.appendChild(text);
-
+    if (txt) {
+        const text = document.createTextNode(txt);
+        element.appendChild(text);
+    }
+    if (atr) {
+        element.setAttribute(atr, atrValue);
+    }
     content.appendChild(element)
 }
 
-formAdd.addEventListener('submit', addNode);
+formAdd.addEventListener('submit', (e) => addNode(e,
+    formAdd.elements.node.value,
+    formAdd.elements.txt.value,
+    formAdd.elements.atr.value,
+    formAdd.elements.atrValue.value
+));
