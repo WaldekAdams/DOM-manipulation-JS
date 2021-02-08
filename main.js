cbef4e9a154add309c2cb08af5929410682fcde1
Waldemar Adams <waldek-adamski@wp.pl>
2021-02-08 07:30:48 +0100

    feat: add fn to search node in DOM

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 const formAdd = document.querySelector('.form--add');
+const formSearch = document.querySelector('.form--serch');
 const content = document.querySelector('.content');
 
+const showInfo = function (elements, divResults) {
+    console.log(elements, divResults);
+}
+
+const searchElements = (e, nameElement) => {
+    e.preventDefault();
+    const elements = document.querySelectorAll(nameElement);
+    const divResults = document.querySelector('.result');
+    // console.log(elements);
+
+    if (elements.length > 0) {
+        divResults.innerHTML = ` <p class = "result__info"> W tym dokumencie znalazlem ${elements.length} elementy ${nameElement}. </p>`
+
+        showInfo(elements, divResults);
+
+    } else {
+        divResults.innerHTML = ` <p class = "result__info"> W tym dokumencie nie znalazlem elementów ${nameElement}. </p>`;
+        return;
+    }
+}
 const addNode = function (e, node, txt, atr, atrValue) {
     e.preventDefault();
     console.log('ok');
@@ -15,6 +36,15 @@ const addNode = function (e, node, txt, atr, atrValue) {
     }
     content.appendChild(element)
 }
+
+
+formSearch.addEventListener('submit', (e) => {
+
+    searchElements(e,
+        formSearch.elements.search.value)
+
+
+})
 
 formAdd.addEventListener('submit', (e) => addNode(e,
     formAdd.elements.node.value,
